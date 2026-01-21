793151bbd0035e64cccff0769e8d7c0c5a4a87e9
no92one <linus.lindroth.92@gmail.com>
2026-01-21 12:04:55 +0100

    added filtering

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import type PokemonCardData from "./interfaces/PokmonCardData.ts";
 
 export default function App() {
   const [pokemonCards, setPokemonCards] = useState<PokemonCardData[]>([])
+  const [cardCategories, setCardCategories] = useState<string[]>([])
+  const [selectedCaregory, setSelectedCategory] = useState<string>("All")
 
   async function getPokemonCardsData() {
     const response = await fetch("/pokemonCards.json")
@@ -15,14 +17,29 @@ export default function App() {
     }
   }
 
+  function getAllCardCategories(){
+    setCardCategories(["All", 
+      ...new Set(pokemonCards.map(card => card.supertype))])
+  } 
+
   useEffect(() => {
     getPokemonCardsData()
     }, []);
 
+  useEffect(() => {
+    getAllCardCategories()
+  }, [pokemonCards])
+
     return <>
       <h1>Min sida</h1>
+      <select onChange={element => setSelectedCategory(element.target.value)}>
+        {cardCategories.map((category, index) => {
+         return <option key={index} value={category}>{category}</option>
+        })}
+      </select>
       <button onClick={() => console.log(pokemonCards)}>Log data</button>
-      {pokemonCards.map( (card, index) => {
+      {pokemonCards.filter(card => card.supertype === selectedCaregory || selectedCaregory === "All")
+      .map( (card, index) => {
         return <section key={index}>
           <p>{card.name}</p>
           <img src={card.images.small} alt="No image" />
--- src/interfaces/PokmonCardData.ts
+++ src/interfaces/PokmonCardData.ts
@@ -3,4 +3,5 @@ import type Images from "./Images.ts";
 export default interface PokemonCardData {
     name: string;
     images: Images;
+    supertype: string;
 }
