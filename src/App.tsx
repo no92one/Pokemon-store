20c1618298bb50b5b6de092ecef658b632148aa9
no92one <linus.lindroth.92@gmail.com>
2026-01-21 11:02:12 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [pokemonCards, setPokemonCards] = useState([])
+
+  async function getPokemonCardsData() {
+    const response = await fetch("/pokemonCards.json")
+    const result = await response.json()
+
+    if(response.ok){
+      setPokemonCards(result)
+    } else {
+      alert("Something went wrong!")
+    }
+  }
+
+  useEffect(() => {
+    getPokemonCardsData()
+    }, []);
+
+    return <>
+      <h1>Min sida</h1>
+      <button onClick={() => console.log(pokemonCards)}>Log data</button>
+      {pokemonCards.map( (card, index) => {
+        return <section key={index}>
+          <p>{card.name}</p>
+        </section>
+      })}
+    </>;
+}
